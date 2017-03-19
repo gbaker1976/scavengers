2d470395833ea09b616f53be21c03bb5cf5a9c6f
Garrett Baker <garrett.baker@gmail.com>
2017-03-19 18:24:09 -0400

    added new files

---
+++ Assets/Scripts/FoodItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FoodItem : Item
+{
+	// the default behavior for items is to be used immediately, as in food/soda.
+	// the Collect method should be used to inventory collecatble items.
+	public override void Collect(IConsumer c)
+	{
+		Use (c);
+		gameObject.SetActive (false);
+	}
+
+
+	public override void Use(IConsumer c)
+	{
+		c.Consume (this);
+	}
+}
---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour {
+
+	private List<IConsumable> loot = new List<IConsumable>();
+
+	public static InventoryManager instance = null;
+	public Player player;
+
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);    
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void AddToInventory(IConsumable c)
+	{
+		loot.Add (c);
+	}
+}
+		
